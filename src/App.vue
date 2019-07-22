fb60bd017440512216354604d825d206cfddd8f6
Preeti <singhp1992@gmail.com>
2019-07-22 13:17:22 +0200

    removed dummy data

--- src/App.vue
+++ src/App.vue
@@ -5,49 +5,6 @@
 </template>
 
 <script>
-const movie = [
-    {
-        title: 'The Lion King',
-        description: "A delightful children's movie.",
-        genre: 'Family',
-        director: 'Karen Gilchrist',
-        rating: 'G',
-        poster: 'https://m.media-amazon.com/images/M/MV5BMTg2MzI1MTg3OF5BMl5BanBnXkFtZTgwNTU3NDA2MTI@._V1_SX300.jpg',
-        showDetail: false,
-    },
-    {
-        title: 'The Lion King II',
-        description: "A delightful children's movie.",
-        genre: 'Family',
-        director: 'Karen Gilchrist',
-        rating: 'G',
-        showDetail: false,
-    },
-    {
-        title: 'Silence of the Lambs',
-        description: "Crazy Cannibal",
-        genre: 'Horror',
-        director: 'Ron Bozman',
-        rating: 'R',
-        showDetail: false
-    },
-    {
-        title: 'Planet Earth II',
-        description: "Hours of beautiful but heart attack-inducing nature footage",
-        genre: 'Documentary',
-        director: 'David Attenborough',
-        rating: 'G',
-        showDetail: false,
-    },
-    {
-        title: 'Titanic',
-        description: "The boat sinks.",
-        genre: 'Romance',
-        director: 'James Cameron',
-        rating: 'PG-13',
-        showDetail: false,
-    }
-]
 import Menu from './components/Menu.vue'
 
 export default {
